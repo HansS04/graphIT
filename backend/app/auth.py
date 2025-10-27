8d02903783b2e7dba3409275a9f5090bd81ca670
HansS04 <jan.slivka.workspace@seznam.cz>
2025-10-27 21:04:50 +0100

    Fixed password encryption

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -14,7 +14,7 @@ if not SECRET_KEY:
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["scrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
